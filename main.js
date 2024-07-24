0a267045b6eefbe81967dde18396d383989401c7
jdluis <jdluisdev@gmail.com>
2024-07-24 20:33:28 +0200

    first steps, creation of a cube and a scene of three

--- main.js
+++ main.js
@@ -1,8 +1,46 @@
 import './style.css'
+import * as THREE from 'three'
 
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Text of index.html
 document.querySelector('#app').innerHTML = `
   <div>
     <h1>Learning Three.js!</h1>
     <p>Showcase of My Skills in Learning Three.js</p>
   </div>
 `
+
+// First Steps Project
+console.log(THREE)
+
+// Scene
+const scene = new THREE.Scene()
+
+// Objects
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 'red' })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 800
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+camera.position.x = 1
+
+scene.add(camera)
+
+// Render
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+renderer.render(scene, camera)
